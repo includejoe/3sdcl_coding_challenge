7548c73fd8c80dbfdd97ba343bf404c6ea081c74
joel ayertey <kvnqjoe@gmail.com>
2023-05-22 23:49:08 +0000

    upload image done

--- app/src/main/java/org/includejoe/sdcl/presentation/composables/Avatar.kt
+++ app/src/main/java/org/includejoe/sdcl/presentation/composables/Avatar.kt
@@ -7,36 +7,45 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import org.includejoe.sdcl.R
 
 @Composable
 fun Avatar(
-    modifier: Modifier = Modifier,
     size: Dp,
-    src: String? = null,
+    src: ImageBitmap? = null,
 ) {
-    Image(
-        painter = if(src !== null) rememberAsyncImagePainter(src)
-        else painterResource(R.drawable.ic_avatar),
-        modifier = modifier
-            .size(size)
-            .clip(shape = RoundedCornerShape(size / 2))
-            .background(
-                MaterialTheme.colors.background,
-                shape = RoundedCornerShape(size / 2)
-            )
-            .layoutId("avatar")
-            .border(
-                border = BorderStroke(width = 3.dp, color = MaterialTheme.colors.primary),
-                shape = RoundedCornerShape(size / 2)
-            ),
-        contentDescription = "avatar",
-        contentScale = ContentScale.Crop,
-    )
+    val modifier: Modifier = Modifier
+        .size(size)
+        .clip(shape = RoundedCornerShape(size / 2))
+        .background(
+            MaterialTheme.colors.background,
+            shape = RoundedCornerShape(size / 2)
+        )
+        .layoutId("avatar")
+        .border(
+            border = BorderStroke(width = 3.dp, color = MaterialTheme.colors.primary),
+            shape = RoundedCornerShape(size / 2)
+        )
+
+    if (src !== null) {
+        Image(
+            bitmap = src,
+            modifier = modifier,
+            contentDescription = "avatar",
+            contentScale = ContentScale.Crop,
+        )
+    } else {
+        Image(
+            painter = painterResource(id = R.drawable.ic_avatar),
+            modifier = modifier,
+            contentDescription = "avatar",
+            contentScale = ContentScale.Crop
+        )
+    }
 }
